f82bfaba8e022dc67dff8c3e4a59e9bf39ffa63f
otyacazyu <143002834+otyacazyu@users.noreply.github.com>
2023-09-02 17:22:40 +0900

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,13 @@ import java.util.List;
 import static java.lang.System.out;
 
 public class Main {
+    
     public static void main(String[] args) {
         List<String> names = List.of("yamada","tanaka","yasuda","suzuki","iida","satou","tatibana");
         List<String> result = names.stream().filter(name -> name.startsWith("t")).toList();
+       
         names.stream().filter(name -> name.length() <= 5).forEach(out::println);
+        
         out.println(result);
     }
         }
